d6b1cd564321b56673aee2965a36787f866bdeac
longqi <longqi90@gmail.com>
2023-02-08 10:29:23 +0800

    export objects as a module

---
+++ sdbus_block/modemmanager/__init__.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from .objects import (
+	MM,
+	MMModems,
+	MMModem,
+	MMModemSimple,
+	MMModemSignal,
+	MMSim,
+	MMBearer,
+)
+
+__all__ = (
+	# .objects
+	'MM',
+	'MMModems',
+	'MMModem',
+	'MMModemSimple',
+	'MMModemSignal',
+	'MMSim',
+	'MMBearer',
+)
