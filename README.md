587d42226cf69d6250d8c425d411a16dfd95deff
longqi <longqi90@gmail.com>
2024-01-16 15:58:18 +0800

    publish to pypi

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # python-sdbus-modemmanager
+
+## ModemManager binds for python-sdbus
+
+Supports both asyncio (under `sdbus_async.ModemManager` module) and blocking (under `sdbus_block.ModemManager` module)
+
+Implements most ModemManager dbus interfaces and objects.
+
+## Requirements
+`python-sdbus` version higher than 0.8rc2
+See [python-sdbus requirements](https://github.com/igo95862/python-sdbus#requirements).
+
+## Installation
+`pip install --only-binary ':all:' sdbus-ModemManager`
+
+## Documentation
+
+This is the sub-project of [python-sdbus](https://github.com/igo95862/python-sdbus).
+
+See the [python-sdbus documentation](https://python-sdbus.readthedocs.io/en/latest/).
--- setup.py
+++ setup.py
@@ -1,29 +1,29 @@
 from setuptools import setup
 
 setup(
-    name='sdbus-modemmanager',
-    description=('ModemManager binds for sdbus.'),
-    version='1.0rc1',
-    url='https://github.com/zhanglongqi/python-sdbus-modemmanager',
-    project_urls={
-        'Source': 'https://github.com/zhanglongqi/python-sdbus-modemmanager/',
-        'Tracker': 'https://github.com/zhanglongqi/python-sdbus-modemmanager/issues/',
-    },
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        (
-            'License :: OSI Approved :: '
-            'GNU Lesser General Public License v2 or later (LGPLv2+)'
-        ),
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
-    license='LGPL-2.1-or-later',
-    packages=['sdbus_async.modemmanager', 'sdbus_block.modemmanager'],
-    python_requires='>=3.7',
-    install_requires=[
-        'sdbus>=0.8rc2',
-    ],
+	name='sdbus-modemmanager',
+	description=('ModemManager binds for sdbus.'),
+	long_description=open('README.md').read(),
+	long_description_content_type='text/markdown',
+	version='1.0.0',
+	url='https://github.com/zhanglongqi/python-sdbus-modemmanager',
+	project_urls={
+		'Source': 'https://github.com/zhanglongqi/python-sdbus-modemmanager/',
+		'Tracker': 'https://github.com/zhanglongqi/python-sdbus-modemmanager/issues/',
+	},
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+		'Intended Audience :: Developers',
+		('License :: OSI Approved :: '
+			'GNU Lesser General Public License v2 or later (LGPLv2+)'),
+		'Operating System :: POSIX :: Linux',
+		'Programming Language :: Python :: 3 :: Only',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+	],
+	license='LGPL-2.1-or-later',
+	packages=['sdbus_async.modemmanager', 'sdbus_block.modemmanager'],
+	python_requires='>=3.7',
+	install_requires=[
+		'sdbus>=0.8rc2',
+	],
 )
