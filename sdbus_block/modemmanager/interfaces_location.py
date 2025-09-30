07592b74d238a1dcb98af4f33adf744dccd808f0
Kevin Barrios <kbarrios@anacorp.com>
2025-09-30 10:14:06 -0700

    method additions

--- sdbus_block/modemmanager/interfaces_location.py
+++ sdbus_block/modemmanager/interfaces_location.py
@@ -4,7 +4,7 @@ from sdbus import DbusInterfaceCommon, dbus_method, dbus_property
 
 
 class MMModemLocationInterface(DbusInterfaceCommon, interface_name='org.freedesktop.ModemManager1.Modem.Location'):
-	"""
+    """
     The Location interface allows devices to provide location information to client applications. 
     Not all devices can provide this information, or even if they do, they may not be able to provide it while a data session is active.
 
@@ -12,16 +12,29 @@ class MMModemLocationInterface(DbusInterfaceCommon, interface_name='org.freedesk
     3GPP devices will require a valid unlocked SIM card before any of the features in the interface can be used (including GNSS module management).
     """
 
-	@dbus_method()
-	def get_location(self) -> dict:
-		"""Return current location information, if any. If the modem supports multiple location types it may return more than one."""
-		raise NotImplementedError
+    @dbus_method()
+    def get_location(self) -> dict:
+        """Return current location information, if any. If the modem supports multiple location types it may return more than one."""
+        raise NotImplementedError
 
-	@dbus_property(property_signature='a{sv}')
-	def location(self) -> List[dict]:
-		"""
+    @dbus_property(property_signature='a{sv}')
+    def location(self) -> List[dict]:
+        """
         Dictionary of available location information when location information gathering is enabled. 
         If the modem supports multiple location types it may return more than one here.
         """
-		raise NotImplementedError
-	
+        raise NotImplementedError
+
+    @dbus_property(property_signature='u')
+    def enabled(self) -> dict:
+        """
+        dict specifying which of the supported MMModemLocationSource location sources are enabled.
+        """
+        raise NotImplementedError
+
+    @dbus_property(property_signature='b')
+    def signals_location(self) -> bool:
+        """
+        True if location updates will be emitted via D-Bus signals, False if location updates will not be emitted.
+        """
+        raise NotImplementedError
